38798806a20eb1fc35f3c74ead4397bf2aaaaf25
Augusto Silva <augusto.a.silva@hotmail.com>
2020-04-16 16:32:48 -0300

    Update Terms and Conditions

--- pages/terms-and-conditions.vue
+++ pages/terms-and-conditions.vue
@@ -4,8 +4,142 @@
       <v-col cols="12" class="text-justify pa-6">
         <h1 class="text-center">Pulmonapp - Web App</h1>
         <h2 class="text-center">Termos e Condições</h2>
+        <br />
         <p>
-          Termos e condições a serem escritos
+          Estes Termos e Condições (doravante denominados “Termos e Condições)
+          regulamenta o uso do serviço da aplicação online, ou aplicativo para
+          web, www.pulmonapp.com (doravante, “APLICATIVO”) que AUGUSTO ALVES
+          SILVA, pessoa física com sede na cidade de Teresina - PI (doravante,
+          "Augusto") e a Equipe Pulmonar, Equipe de tecnologia trabalhando na
+          adaptação de baixo custo de respiradores mecânicos para pacientes do
+          COVID-19 (doravante, "Pulmonar") liderados por CAIO DAMASCENO
+          (doravante, “Caio”) oferece aos seus USUÁRIOS.
+        </p>
+        <p>
+          1) Profissionais da área da saúde que estejam no tratamento direto de
+          pacientes do COVID-19, pessoas física, doravante nominada USUÁRIO, que
+          pretenda utilizar os serviços do APLICATIVO, deverá aceitar as
+          Cláusulas de Uso e todas as demais políticas e princípios que as
+          regem.
+        </p>
+        <p>
+          2) A ACEITAÇÃO DESTES TERMOS E CONDIÇÕES GERAIS É INDISPENSÁVEL À
+          UTILIZAÇÃO DOS SITES E SERVIÇOS PRESTADOS PELO APLICATIVO. O USUÁRIO
+          deverá ler, certificar-se de haver entendido e aceitar todas as
+          disposições estabelecidas nos Termos e Condições e na Política de
+          Privacidade, para que então seja efetuado com sucesso seu cadastro
+          como USUÁRIO do APLICATIVO.
+        </p>
+        <h2>Do Usuário:</h2>
+        <P>
+          3) Os serviços do APLICATIVO somente estão disponíveis para as pessoas
+          que sejam profissionais das seguintes áres da saúde: médicos,
+          enfermeiros, fisioterapeutas e farmaceutícos que estejam trabalhando
+          atualmente no tratamento de pacientes que sofrem da pandemia do
+          COVID-19.
+        </P>
+        <p>
+          4) O USUÁRIO se compromete a fornecer seus dados pessoais de forma
+          verdadeira e precisa, devendo atualizá-los sempre que ocorrer alguma
+          alteração.
+        </p>
+        <p>
+          <strong>
+            5) O APLICATIVO não se responsabiliza pela veracidade das
+            informações prestadas por seus USUÁRIOS, sendo deles toda a
+            responsabilidade por seu conteúdo.
+          </strong>
+        </p>
+        <p>
+          7) O APLICATIVO pode checar a veracidade dos dados cadastrais de um
+          USUÁRIO a qualquer tempo. Caso constate haver entre eles dados
+          incorretos ou inverídicos, ou ainda caso o USUÁRIO se furte ou se
+          negue a enviar os documentos requeridos, o APLICATIVO poderá bloquear
+          o perfil do USUÁRIO, até que a irregularidade seja sanada, ou até
+          mesmo excluir o perfil do USUÁRIO caso seja julgado necessário pelo
+          APLICATIVO.
+        </p>
+        <p>
+          8) O USUÁRIO acessará sua conta através de login e senha,
+          comprometendo-se a não informar a terceiros esses dados,
+          responsabilizando-se integralmente pelo uso que deles seja feito.
+        </p>
+        <p>
+          9) O USUÁRIO compromete-se a notificar o APLICATIVO imediatamente,
+          através de comunicação em que fique certificado o recebimento pelo
+          APLICATIVO, a respeito de qualquer uso não autorizado de sua conta,
+          bem como o acesso não autorizado por terceiros a mesma. O USUÁRIO será
+          o único responsável pelas operações efetuadas em sua conta, uma vez
+          que o acesso à mesma só será possível mediante a senha pessoal do
+          USUÁRIO.
+        </p>
+        <p>
+          10) O login que o USUÁRIO utiliza no APLICATIVO não poderá guardar
+          semelhança com o nome Pulmonapp, Pulmonar, e seus derivados, tampouco
+          poderá ser utilizado qualquer login considerado ofensivo, bem como os
+          que contenham dados pessoais do USUÁRIO ou alguma URL ou endereço
+          eletrônico.
+        </p>
+        <h2>Do Serviço Prestado:</h2>
+        <p>
+          11) O APLICATIVO é um website que possui como objetivo auxiliar os
+          USUÁRIOS ,profissionais da saúde, na hora de calcular o volume
+          corrente a ser aplicado nos pacientes através de ventilação mecânica a
+          serem tratados devido a pandemia do COVID-19, bem como manter dados
+          dos pacientes e um histórico.
+        </p>
+        <p>
+          12) O serviços disponibilizados pelo APLICATIVO são GRATUITOS.
+        </p>
+        <p>
+          13) O APLICATIVO não da garantia ou se responsabiliza por
+          instabilidade, erros de código, que podem vir a prejudicar o uso pelo
+          USUÁRIO.
+        </p>
+        <h2>Considerações finais:</h2>
+        <p>
+          14) O conteúdo deste instrumento poderá ser modificado livremente pelo
+          APLICATIVO por meio de comunicação eletrônica aos USUÁRIOS, passando
+          os novos termos a valer desde a data em que forem veiculados no site,
+          reservando-se ao USUÁRIO o direito de permanecer cadastrado ou
+          simplesmente solicitar sua exclusão.
+        </p>
+        <p>
+          15) Todo o conteúdo, como, por exemplo, textos, programas de
+          computador, sons, fotografias e outros materiais disponíveis no
+          APLICATIVO, marcas, patentes, modelos, desenhos industriais, a
+          aparência, a organização e a estrutura do APLICATIVO são protegidos
+          pela legislação de propriedade intelectual aplicável e não infringem
+          qualquer lei ou norma a que estejam subordinadas, contratos,
+          documentos, acordos dos quais faz parte, bem como não infringe
+          direitos de terceiros. A violação de tais direitos ensejará a
+          respectiva reparação integral dos danos aos prejudicados, seja a
+          APLICATIVO, seus USUÁRIOS e/ou terceiros.
+        </p>
+        <p>
+          16) É permitido ao USUÁRIO modificar, copiar, distribuir, transmitir,
+          exibir, realizar, reproduzir, publicar, disponibilizar, licenciar ou
+          criar obras derivadas a partir das informações coletadas no
+          APLICATIVO, bem como transferir ou vender tais informações, software,
+          produtos ou serviços, de tal maneira que a licença
+          <a
+            href="https://github.com/augustoasilva/pulmonapp-web-app/blob/master/LICENSE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GNU - GPLv3
+          </a>
+          sobre o qual o APLICATIVO está disponibilizado seja respeitada.
+        </p>
+        <p>
+          17) Fica eleito, para dirimir eventuais dúvidas e resolver os
+          conflitos oriundos deste contrato, o foro da Cidade de Teresina,
+          Estado do Piauí, com renúncia a qualquer outro, por mais privilegiado
+          que seja.
+        </p>
+        <p>
+          17) Os casos omissos neste instrumento serão resolvidos de
+          conformidade com as disposições legais aplicáveis à espécie.
         </p>
       </v-col>
     </v-row>
